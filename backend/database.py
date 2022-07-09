536fa6ceaf50540e79239686e9c65ce6dad0f46b
Jiacheng <e0773814@u.nus.edu>
2022-07-10 00:27:38 +0800

    added database queries for company listings and insert queries for companies

--- backend/database.py
+++ backend/database.py
@@ -55,4 +55,62 @@ class Database:
             print(e)
             return e
 
+def insert_branch(self, branchName, companyName, branchAddress, branchArea, password):
+            try:
+                LOCK.acquire(True)
+                ## if branch and company name exists, do not insert
+                self.cur.execute("SELECT * FROM Branches WHERE CompanyName=? AND BranchName=?",(companyName, branchName,))
+                if (len(self.cur.fetchall())):
+                    return False
+
+                self.cur.execute("INSERT INTO Branches(BranchName, CompanyName, BranchAddress, BranchArea, BranchPassword) values (?,?,?,?,?)",
+                                (branchName, companyName, branchAddress, branchArea, password,))
+                self.con.commit()
+                return True
+            except Exception as e:
+                print(e)
+                return e
+            finally:
+                LOCK.release()
+
+    def query_branchID(self, companyName, branchName):
+        try:
+            LOCK.acquire(True)
+            self.cur.execute("SELECT * FROM Branches WHERE CompanyName=? AND BranchName=?", (companyName, branchName,))
+            self.con.commit()
+            rows = self.cur.fetchall()
+            #only interested in the first row returned
+            #branch ID is the first item in the row
+            branch_id = rows[0][0]
+            print(branch_id)
+            return branch_id
+
+        except Exception as e:
+            print(e)
+            return e
+        finally:
+            LOCK.release()
+
+    def query_company_listings(self, branchID):
+        try:
+            LOCK.acquire(True)
+            self.cur.execute("SELECT * FROM Listings WHERE BranchID=?", (branchID,))
+            self.con.commit()
+            rows = self.cur.fetchall()
+            if not (rows):
+                return
+            
+            listingArr = []
+
+            for row in rows:
+                listingArr.append(row)
+            
+            print(listingArr)
+            return listingArr
+            
+        except Exception as e:
+            print(e)
+            return e
+        finally:
+            LOCK.release()
 
