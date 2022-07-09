27b0a71afccc7edaa8b3cd491629f9a781356523
Jiacheng <e0773814@u.nus.edu>
2022-07-10 00:22:37 +0800

    edited data base columns

--- backend/database.py
+++ backend/database.py
@@ -33,22 +33,20 @@ class Database:
                 '''CREATE TABLE Branches(
                     BranchID integer primary key, 
                     BranchName text, 
-                    CompanyID integer not null,
+                    CompanyName text,
                     BranchAddress text, 
                     BranchArea text, 
                     BranchPassword text, 
-                    FORGEIGN KEY(CompanyID) REFERENCES Companies(CompanyID), 
                 )''')
             self.cur.execute(
                 '''CREATE TABLE Listings(
                     ListingID integer primary key, 
                     ListingName text, 
                     ListingDescription text, 
-                    ListingPrice, 
-                    ListingCompanyID integer not null, 
+                    ListingPrice DECIMAL(6, 2), 
+                    ListingCompanyName text, 
                     ListingBranchID integer not null, 
                     ListingImage BLOB, 
-                    FOREIGN KEY(ListingCompanyID) REFERENCES Companies(CompanyID),
                     FOREIGN KEY(ListingBranchID) REFERENCES Branches(BranchID)
                 )''')
             self.con.commit()
@@ -57,5 +55,62 @@ class Database:
             print(e)
             return e
 
+    def insert_branch(self, branchName, companyName, branchAddress, branchArea, password):
+            try:
+                LOCK.acquire(True)
+                ## if branch and company name exists, do not insert
+                self.cur.execute("SELECT * FROM Branches WHERE CompanyName=? AND BranchName=?",(companyName, branchName,))
+                if (len(self.cur.fetchall())):
+                    return False
 
+                self.cur.execute("INSERT INTO Branches(BranchName, CompanyName, BranchAddress, BranchArea, BranchPassword) values (?,?,?,?,?)",
+                                (branchName, companyName, branchAddress, branchArea, password,))
+                self.con.commit()
+                return True
+            except Exception as e:
+                print(e)
+                return e
+            finally:
+                LOCK.release()
+
+    def query_branchID(self, companyName, branchName):
+        try:
+            LOCK.acquire(True)
+            self.cur.execute("SELECT * FROM Branches WHERE CompanyName=? AND BranchName=?", (companyName, branchName,))
+            self.con.commit()
+            rows = self.cur.fetchall()
+            #only interested in the first row returned
+            #branch ID is the first item in the row
+            branch_id = rows[0][0]
+            print(branch_id)
+            return branch_id
+
+        except Exception as e:
+            print(e)
+            return e
+        finally:
+            LOCK.release()
+
+    def query_company_listings(self, branchID):
+        try:
+            LOCK.acquire(True)
+            self.cur.execute("SELECT * FROM Listings WHERE BranchID=?", (branchID,))
+            self.con.commit()
+            rows = self.cur.fetchall()
+            if not (rows):
+                return
+            
+            listingArr = []
+
+            for row in rows:
+                listingArr.append(row)
+            
+            print(listingArr)
+            return listingArr
+            
+        except Exception as e:
+            print(e)
+            return e
+        finally:
+            LOCK.release()
 
